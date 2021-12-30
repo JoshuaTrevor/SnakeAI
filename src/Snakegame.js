0227623fba8d3e632d2e608ec502dd918bfb86fd
Joshua Trevor <u6405233@anu.edu.au>
2021-12-30 21:31:17 +1100

    Apple eating and growth

--- src/Snakegame.js
+++ src/Snakegame.js
@@ -7,6 +7,8 @@ const initialState = {
   grid: [],
   x: 4,
   y: 4,
+  foodX: 6,
+  foodY: 6,
   bodyCoords: [[4, 4], [4, 5]]
 };
 
@@ -16,7 +18,8 @@ class Snakegame extends React.Component
   {
     super();
     this.state = initialState;
-
+    this.state.foodX = Math.floor(Math.random()*10);
+    this.state.foodY = Math.floor(Math.random()*10);
     const grid = [];
     for (let row = 0; row < this.state.rows; row++) {
       for (let col = 0; col < this.state.cols; col++) {
@@ -29,12 +32,23 @@ class Snakegame extends React.Component
     this.state.grid = grid;
     this.keyPressed = this.keyPressed.bind(this)
     this.bodyContains = this.bodyContains.bind(this)
+    this.getSquareClass = this.getSquareClass.bind(this)
 
     console.log(this.state)
+  }
+
+  getSquareClass(x, y)
+  {
+    if(this.bodyContains(x, y))
+      return "grid-item snake";
+    else if(x === this.state.foodX && y === this.state.foodY)
+      return "grid-item food"
+    else return "grid-item"
   }
   
   keyPressed(event)
   {
+    var appleEaten = false;
     var newHeadX = this.state.x;
     var newHeadY = this.state.y;
     //To move, add a piece in the direction moved, and if there is not food there delete the tail
@@ -57,19 +71,48 @@ class Snakegame extends React.Component
         break;
     }
 
+    var newFoodX = this.state.foodX
+    var newFoodY = this.state.foodY
+
+    //If snake collides with apple, replace it
+    if(newHeadX === this.state.foodX && newHeadY === this.state.foodY)
+    {
+      appleEaten = true;
+
+      //Continue trying to place the apple until it is not inside the snake. 
+      //Inefficient and poor solution, will be replaced later.
+      var shouldExit = false;
+      while(!shouldExit)
+      {
+        newFoodX = Math.floor(Math.random()*10);
+        newFoodY = Math.floor(Math.random()*10);
+        shouldExit = !this.bodyContains(newFoodX, newFoodY);
+      }
+    }
     //If should die check here
+    if(this.bodyContains(newHeadX, newHeadY) || newHeadX >= 10 || newHeadX < 0 || newHeadY >= 10 || newHeadY < 0)
+    {
+      this.setState(initialState);
+      this.setState({bodyCoords : [[4, 4], [4,5]]})
+    }
 
     //If should not die then make the move
-
-    this.state.bodyCoords.pop();
-    this.state.bodyCoords.unshift([newHeadX, newHeadY])
+    else
+    {
+      if(!appleEaten)
+      {
+        this.state.bodyCoords.pop();
+      }
+      this.state.bodyCoords.unshift([newHeadX, newHeadY])
     
-    this.setState({
-      x : newHeadX,
-      y : newHeadY
-    })
+      this.setState({
+        x : newHeadX,
+        y : newHeadY,
+        foodX : newFoodX,
+        foodY : newFoodY
+      })
+    }
     console.log(this.state.bodyCoords)
-    console.log(this.state.x + "," + this.state.y)
   }
 
   //Check if the snake contains certain coordinates, used for rendering snake
@@ -91,7 +134,7 @@ class Snakegame extends React.Component
         x={grid.col} 
         y={grid.row}
         key={grid.row.toString() + ":" + grid.col.toString()}
-        className={this.bodyContains(grid.col, grid.row) ? "grid-item snake" : "grid-item"} ></div>
+        className={this.getSquareClass(grid.col, grid.row)} ></div>
     })
     
     
